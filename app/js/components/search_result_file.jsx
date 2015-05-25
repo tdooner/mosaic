9b3ac7373f8bbd10c30e9314f7b385b6dc86b3b7
Joe Lencioni <joe.lencioni@brigade.com>
2015-05-25 11:11:32 -0700

    Fix missing `key` warnings in <SearchResultFile>
    Whenever an array is rendered in React, you need to give each member a
    key prop so that React can keep track of them and do its reconciliation
    properly. Without these props, at best React will be slower; at worst
    you will end up with some really strange rendering problems.
    
    I resolved one by giving each item a unique key and the other by
    removing the need for an array altogether. This makes the code more
    readable.

--- app/js/components/search_result_file.jsx
+++ app/js/components/search_result_file.jsx
@@ -4,29 +4,32 @@ const React = require('react'),
 
 const SearchResultFile = React.createClass({
   render() {
-    const result = this.props.result;
-
-    result.basename = result.file.split('/').pop();
-    const result_info = [];
-    result_info.push(
-      <span>
-        <i className="fa fa-clock-o" />
-        {' ' + moment(result.last_modified).fromNow()}
-        {' \u00b7 '}
-      </span>
-    );
-    if (result.tags.length > 0) {
-      result_info.push(<span>{result.tags.join(',')}</span>);
-    }
-    result_info.push(<span> <a target='_new' href={'/download/' + result.file_id}>Download from Dropbox</a></span>);
+    const { result } = this.props;
+    const resultBasename = result.file.split('/').pop();
 
     return (
       <div className="result-file row">
         <div className="result-filename-container">
-          <h2 className="result-filename"><i className="fa fa-file-image-o" /> {result.basename}</h2>
-          {result_info}
+          <h2 className="result-filename">
+            <i className="fa fa-file-image-o" />
+            {' '}
+            {resultBasename}
+          </h2>
+          <i className="fa fa-clock-o" />
+          {' ' + moment(result.last_modified).fromNow()}
+          {' \u00b7 '}
+          {(result.tags.length > 0) && result.tags.join(',')}
+          <a target='_new' href={`/download/${result.file_id}`}>
+            Download from Dropbox
+          </a>
         </div>
-        {result.slices.map(slice => <SearchResultSlice slice={slice} />)}
+
+        {result.slices.map(
+          slice => <SearchResultSlice
+            key={`${slice.sketch_file_id}${slice.path}${slice.layer}`}
+            slice={slice}
+          />
+        )}
       </div>
     );
   }
